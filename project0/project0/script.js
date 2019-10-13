2b80af03e4dc14b6954b082bff8749d7d02c75ea
erikbenton <erik.benton@hotmail.com>
2019-10-13 08:21:07 -0400

    adding text field with enter press to submit

--- project0/project0/script.js
+++ project0/project0/script.js
@@ -8,18 +8,39 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
+const todoText = document.getElementById("todo-text")
 
 function newTodo() {
-  let task = prompt('What is your new "todo" task?')
+  let task = document.getElementById("todo-text")
 
-  // Get teh list object
+  // Check if there is actually any text in the input
+  if(task.value.length < 1) {
+  	alert("You must enter a task into the text field")
+  	return
+  }
+
+  // Get the list object
   let ul = document.getElementById('todo-list')
 
+  addTaskToList(ul, task.value)
+
+  task.value = ""
+  return
+}
+
+function addTaskToList(htmlList, taskText) {
   // Create a node for the list with the text from the prompt
   let li = document.createElement("li")
 
   // Append it to the list node and list
-  li.appendChild(document.createTextNode(task))
-  ul.appendChild(li)
-
+  li.appendChild(document.createTextNode(taskText))
+  htmlList.appendChild(li)
+  return
 }
+
+todoText.addEventListener('keydown', function onEvent(e) {
+	if(e.keyCode === 13) {
+		newTodo()
+	}
+	return
+})
